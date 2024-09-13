645e18434bf0b28278d20fc0317c3d6e786820a5
LoHertel <LoHertel@users.noreply.github.com>
2024-09-13 08:54:36 +0200

    feat: change repo metadata asset tasks to simpler logic

--- github_pipeline/assets.py
+++ github_pipeline/assets.py
@@ -1,53 +1,52 @@
 from typing import Any
+
 from dagster import (
     AssetExecutionContext,
-    Backoff,
-    Jitter,
+    FreshnessPolicy,
     MetadataValue,
-    RetryPolicy,
     asset,
 )
 
 from .resources import GitHubAPIResource
 
 
-# asset factory function
-def create_repo_metadata_assets(owner: str, repo: str):
-    @asset(
-        name=repo,
-        key_prefix=['stage', 'github', 'repositories', owner],
-        io_manager_key='json_io_manager',
-        group_name='github',
-        retry_policy=RetryPolicy(max_retries=3, delay=2, backoff=Backoff.EXPONENTIAL, jitter=Jitter.FULL),
-        op_tags={'activate_versioning': True},
+@asset(
+    name='delta-rs_repo_metadata',
+    key_prefix=['stage', 'github', 'repositories', 'delta-io', 'delta-rs'],
+    io_manager_key='json_io_manager',
+    group_name='github',
+    freshness_policy=FreshnessPolicy(maximum_lag_minutes=60 * 24),  # 24 hours
+)
+def delta_rs_metadata(context: AssetExecutionContext, github_api: GitHubAPIResource) -> dict[str, Any]:
+    """Metadata from the GitHub repository of the Delta Lake Python client."""
+    repo_metadata = github_api.get_repository(owner='delta-io', repo='delta-rs')
+
+    context.add_output_metadata(
+        metadata={
+            'repo link': MetadataValue.url(repo_metadata.get('html_url')),
+            'data preview': MetadataValue.json(repo_metadata),
+        }
     )
-    def _repo_metadata(context: AssetExecutionContext, github_api: GitHubAPIResource) -> dict[str, Any]:
-        """Gets metadata for the GitHub repository."""
-        repo_metadata = github_api.get_repository(owner=owner, repo=repo)
 
-        context.add_output_metadata(
-            metadata={
-                'repo link': MetadataValue.url(repo_metadata.get('html_url')),
-                'data preview': MetadataValue.json(repo_metadata),
-            }
-        )
-
-        return repo_metadata
-
-    return _repo_metadata
+    return repo_metadata
 
 
-repositories = [
-    {
-        'owner': 'delta-io',
-        'name': 'delta-rs',
-    },
-    {
-        'owner': 'apache',
-        'name': 'iceberg-python',
-    },
-]
+@asset(
+    name='iceberg-python_repo_metadata',
+    key_prefix=['stage', 'github', 'repositories', 'apache', 'iceberg-python'],
+    io_manager_key='json_io_manager',
+    group_name='github',
+    freshness_policy=FreshnessPolicy(maximum_lag_minutes=60 * 24),  # 24 hours
+)
+def iceberg_python_metadata(context: AssetExecutionContext, github_api: GitHubAPIResource) -> dict[str, Any]:
+    """Metadata from the GitHub repository of the Iceberg Python client."""
+    repo_metadata = github_api.get_repository(owner='apache', repo='iceberg-python')
 
-assets = [
-    create_repo_metadata_assets(owner=repository['owner'], repo=repository['name']) for repository in repositories
-]
+    context.add_output_metadata(
+        metadata={
+            'repo link': MetadataValue.url(repo_metadata.get('html_url')),
+            'data preview': MetadataValue.json(repo_metadata),
+        }
+    )
+
+    return repo_metadata
